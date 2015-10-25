aae0c7b830fecb19ad1810da83c511c03e82ce97
Kamila K <ippanemail@gmail.com>
2015-10-25 12:18:12 -0700

    submission of assignment 2, comment lines add

--- cachematrix.R
+++ cachematrix.R
@@ -25,12 +25,12 @@ cacheSolve <- function(x, ...) {
     m <- x$getinverse()
     
     ## If the inverse has already been calculated (and the matrix has not 
-    ## changed), then the cachesolve should retrieve the inverse from the cache.
+    ## changed), then the cachesolve retrieves the inverse from the cache.
     
     if(!is.null(m)) {
         message("getting cached data")
-        return(m) ## Return a matrix that is the inverse of 'x'
-    }    
+        return(m) 
+    } 
     data <- x$get()
     m <- solve(data, ...)
     x$setinverse(m)
